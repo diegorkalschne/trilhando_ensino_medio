864467d02835d875e2f8445ea3bd33c2b22d9a7f
Diego Kalschne <diegokalschne789@gmail.com>
2024-05-06 21:56:32 -0300

    feat: menu do jogo - início

---
+++ scripts/menu.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+
+func _ready():
+	$Comecar.grab_focus()
+
+#Função para ação de "Começar" o jogo
+func _on_comecar_pressed():
+	pass # Replace with function body.
+	
+#Função para ir para a cena de "Ajuda"
+func _on_ajuda_pressed():
+	get_tree().change_scene_to_file("res://scenes/ajuda.tscn")
+
+#Função para sair do jogo
+func _on_sair_pressed():
+	get_tree().quit()
